11339719480b6de3567df161b20ebbbbde40aa57
vannl <vannl@nhacpro.info>
2022-01-09 23:22:16 +0700

    commit

--- scripts.js
+++ scripts.js
@@ -1,27 +1,25 @@
 const toggleCartBox = () => {
-  const cartBox = document.querySelector(".cart-box");
-  const btnCart = document.querySelector(".my-cart");
-  cartBox.classList.toggle("hide");
-  btnCart.classList.toggle("hide");
+    const cartBox = document.querySelector(".cart-box");
+    const btnCart = document.querySelector(".btn--my-cart");
+    cartBox.classList.toggle("hide");
+    btnCart.classList.toggle("hide");
 };
 
 const allBtnAddCart = document.querySelectorAll(".btn-add-to-cart");
 allBtnAddCart.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    const thisButton = event.target;
-    thisButton.innerText = thisButton.classList.contains("added-cart")
-      ? "add to cart"
-      : "remove from cart";
-    thisButton.classList.toggle("added-cart");
-  });
+    button.addEventListener("click", (event) => {
+        const thisButton = event.target;
+        thisButton.innerText = thisButton.classList.contains("btn--added-cart") ?
+            "add to cart" :
+            "remove from cart";
+        thisButton.classList.toggle("btn--added-cart");
+    });
 });
 
-// document
-//   .querySelectorAll(".btn-add-to-cart")
-//   .addEventListener("click", (event) => {
-//     const thisButton = event.target;
-//     thisButton.innerText = thisButton.classList.contains("added-cart")
-//       ? "add to cart"
-//       : "remove from cart";
-//     thisButton.classList.toggle("added-cart");
-//   });
+const openNav = () => {
+    document.getElementById("menu-sidebar").style.width = "300px";
+};
+
+const closeNav = () => {
+    document.getElementById("menu-sidebar").style.width = "0";
+};
